06d3c8e02a383739b5e79416f99eb241efbfb707
Francesco Paolo Passaro <francesco.paolo.passaro@outlook.com>
2017-10-09 13:54:46 +0200

    AllGrant.sql
    this query / function server to entrust the grants to one or more users depending on the type of object of the source schema, very useful for applying the grant to a new schema to an already existing

---
+++ AllGrant.sql
@@ -0,0 +1,17 @@
+declare
+cursor c is SELECT DISTINCT b.OBJECT_NAME
+  FROM ALL_OBJECTS b
+ WHERE b.OBJECT_TYPE = 'TABLE'
+   AND b.OWNER = 'OWNER_NAME'
+   AND b.OBJECT_TYPE ='TABLE';
+cmd varchar2(200);
+begin
+
+for c1 in c loop
+cmd := 'GRANT SELECT, INSERT, UPDATE, DELETE ON '|| c1.OBJECT_NAME|| ' TO TO_OWNER_NAME';
+execute immediate cmd;
+end loop;
+end;
+
+
+-- OBJECT_TYPE you can change this value
